d39e3ebac07beb546fcf56d524e26db5110e62db
gin <215129113@qq.com>
2021-11-30 19:24:55 +0800

    refactor(getlist): 修改获取列表的查询条件

--- src/services/common.service.js
+++ src/services/common.service.js
@@ -15,8 +15,31 @@ class CommonService {
   }
 
   async getList (ctx) {
-    const result = await mongoose.model(ctx.Model).find({}, { password: 0 })
-    return result
+    const { query, pageSize, currentPage } = ctx.request.query
+
+    let queryCriter
+    if (query) {
+      if (ctx.Model === 'User' || ctx.Model === 'Admin') {
+        queryCriter = {
+          username: { $regex: query, $options: '$i' }
+        }
+      } else {
+        queryCriter = {
+          name: { $regex: query, $options: '$i' }
+        }
+      }
+    } else {
+      queryCriter = {}
+    }
+
+    const total = await mongoose.model(ctx.Model).find(queryCriter).count()
+    const list = await mongoose
+      .model(ctx.Model)
+      .find(queryCriter, { password: 0 })
+      .skip((currentPage - 1) * pageSize)
+      .limit(parseInt(pageSize))
+
+    return { total, list }
   }
 }
 
