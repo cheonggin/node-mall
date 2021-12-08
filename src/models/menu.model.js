3becf6094a6f4960af9d02e32b12dd1b7e95b4b9
gin <215129113@qq.com>
2021-12-08 14:44:53 +0800

    feat(menu): 修改menu模型

--- src/models/menu.model.js
+++ src/models/menu.model.js
@@ -10,22 +10,10 @@ const schema = new mongoose.Schema(
     type: { type: Number, default: 1 }, // 菜单类型，1为菜单，2为按钮
     path: { type: String, trim: true },
     status: { type: Boolean, default: true },
-    parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Menu' }
+    parent: { type: [mongoose.SchemaTypes.ObjectId], default: [null] }
   },
   { timestamps: { createdAt: 'created', updatedAt: 'updated' } }
 )
-
-schema.virtual('children', {
-  localField: '_id',
-  foreignField: 'parent',
-  justOne: false,
-  ref: 'Menu'
-})
-
-schema.pre('find', function (next) {
-  this.populate('parent')
-  next()
-})
 
 const Menu = mongoose.model('Menu', schema)
 
