27b92a5d55cb62093eb1febe29819f12d2487538
gin <215129113@qq.com>
2021-12-09 13:26:02 +0800

    feat(public): 访问静态资源

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,15 @@
+const path = require('path')
 const Koa = require('koa')
 const KoaBody = require('koa-body')
+const resource = require('koa-static')
 
 require('./db')
 const router = require('../router')
 const errorHandle = require('./error-handle')
 const app = new Koa()
 
+// 访问静态资源
+app.use(resource(path.join(__dirname, '../public')))
 // 解析请求参数
 app.use(KoaBody())
 router(app)
