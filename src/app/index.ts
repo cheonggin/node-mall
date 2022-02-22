4dd8899158a9818d9132a0d610f0f54bd76816c2
gin <215129113@qq.com>
2022-02-22 20:55:40 +0800

    feat(response handler): 统一返回格式及错误处理

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,27 @@
 import Koa from 'koa'
 
+import type { Context } from 'koa'
+
 import config from './config'
+import {
+  responseHandler,
+  errorHandler
+} from '../middleware/response.middleware'
 
 const app = new Koa()
+
+app.use(responseHandler())
+app.use(errorHandler())
+
+app.on('error', (err: any, ctx: Context) => {
+  if (ctx) {
+    ctx.status = 500
+    ctx.body = {
+      code: 9999,
+      message: `程序运行时报错：${err.message}`
+    }
+  }
+})
 
 app.listen(config.APP_PORT, () =>
   console.log(`API is listening on ${config.APP_HOST}:${config.APP_PORT}`)
