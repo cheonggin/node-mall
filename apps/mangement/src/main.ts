8e254eb42a003ad8db0bb3037735ef9dda10f2e4
gin <215129113@qq.com>
2022-04-06 21:01:30 +0800

    feat(config): 配置接口文档swagger

--- apps/mangement/src/main.ts
+++ apps/mangement/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { MangementModule } from './mangement.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(MangementModule)
   const PORT = process.env.MANGEMENT_APP_PORT || 3000
+  const options = new DocumentBuilder()
+    .setTitle('商城后台管理系统API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+
+  SwaggerModule.setup('api-docs', app, document)
   await app.listen(PORT)
+  console.log(`api接口文档请访问，http://localhost:${PORT}/api-docs`)
 }
 bootstrap()
