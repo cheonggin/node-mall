0ab85fa68585a15a3700b21a866dc8fa57f5a17a
gin <215129113@qq.com>
2022-04-18 16:22:46 +0800

    feat(web/aut): jwt验证

--- apps/web/src/auth/auth.module.ts
+++ apps/web/src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service'
 import { UserModule } from '../user/user.module'
 import { PassportModule } from '@nestjs/passport'
 import { LocalStragety } from './strageties/local.stragety'
+import { JwtStragety } from '@libs/common/strategies/jwt.stragety'
 
 @Module({
   imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStragety]
+  providers: [AuthService, LocalStragety, JwtStragety]
 })
 export class AuthModule {}
